55dd89c8654419821e970e474114259f7bf00898
David CAVANILLAS <114383991+Cdavid974@users.noreply.github.com>
2026-07-19 10:57:36 +0400

    new file:   .vscode/settings.json 	modified:   index.php 	new file:   style.css

--- index.php
+++ index.php
@@ -4,11 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Porfolio</title>
+    <title>Mon Porfolio de David CVS</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
+    <header class="site-header">
+        <div class="logo">
+            <a href="index.php">David CVS</a>
+        </div>
+        <nav class="main-nav">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Mes Projets</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Créer un projet</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
 </body>
 
 </html>
